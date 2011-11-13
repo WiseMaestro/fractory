cde7fbf83ffc393c335d19fe3ccb2f3eda03e4e8
Keith Wyss <WyssMan@gmail.com>
2011-11-13 03:22:01 -0500

    First commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject fractory "1.0.0-SNAPSHOT"
+  :description "Fractory: A clojure library for generating and coloring fractals in swing"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/fractory/test/core.clj
@@ -0,0 +1,6 @@
+(ns fractory.test.core
+  (:use [fractory.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
